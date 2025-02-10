02091488121a78fc90defe759aaa804417b87ad4
Hannah Manes <baixinhamanes@hotmail.com>
2025-02-09 21:42:57 -0300

    só mais uns ajustes

--- script.js
+++ script.js
@@ -8,55 +8,70 @@ form.addEventListener("submit", (event) => {
 })
 
 function addItem(){
-    const itemList = document.querySelector("ul")
-    const newItem = document.createElement("li")
-    const itemName = document.createElement("span")
-    const deleteButton = document.createElement("button")
-    const deleteIcon = document.createElement("img")
-    const checkbox = document.createElement("input")
-
-    const itemText = input.value
-
-    checkbox.id = "checkbox"
-    checkbox.type = "checkbox"
-
-    itemName.textContent = itemText
-    newItem.classList.add("item")
-    
-    deleteButton.id = "remove-button"
-    deleteButton.type = "button"
-
-    deleteIcon.src = "assets/icon-delete.png"  
-    deleteIcon.id = "delete-icon"
-    deleteIcon.alt = "delete icon"
-
-    deleteButton.append(deleteIcon)
-    newItem.append(checkbox, itemName, deleteButton)
-    itemList.append(newItem)
-    
+    try {
+        const itemList = document.querySelector("ul")
+        const newItem = document.createElement("li")
+        const itemName = document.createElement("span")
+        const inputDiv = document.createElement("div")
+        const deleteButton = document.createElement("button")
+        const deleteIcon = document.createElement("img")
+        const checkbox = document.createElement("input")
         
-    deleteIcon.onclick = (event) => removedItem(event.target)
+        const itemText = input.value
+        
+        inputDiv.classList.add("input-group")
+    
+        checkbox.id = "checkbox"
+        checkbox.type = "checkbox"
+        
+        itemName.textContent = itemText
+        itemName.classList.add("item-description")
+        newItem.classList.add("item")
+        
+        deleteButton.id = "remove-button"
+        deleteButton.type = "button"
+        
+        deleteIcon.src = "assets/icon-delete.svg"  
+        deleteIcon.id = "delete-icon"
+        deleteIcon.alt = "delete icon"
+        
+        deleteButton.append(deleteIcon)
+        inputDiv.append(checkbox, itemName)
+        newItem.append(inputDiv, deleteButton)
+        itemList.append(newItem)
+        
+        deleteIcon.addEventListener("click", function (event) {
+            newItem.id = "removal-style"
+            removedItem(event.target)
+        })
+        clearForm()
+    } catch (error) {
+        console.log("Impossível adicionar item. Tente novamente!")
+    }
 }
 
 function removedItem(button) {
     const listItem = button.closest(".item")
     const listTitleElement = listItem.querySelector("span")
     const listTitle = listTitleElement.textContent
-
+    
+    const removeDiv = document.createElement("div")
     const removalText = document.createElement("span")
     const xIcon = document.createElement("img")
     const warningIcon = document.createElement("img")
 
-    listItem.innerHTML = ""
-    removalText.innerHTML = `${listTitle} foi removido`
+    removeDiv.classList.add("input-group")
 
-    warningIcon.src = "assets/warning.png"  
-    warningIcon.classList.add("removed-item-icon")
+    listItem.innerHTML = ""
+    removalText.innerHTML = `${listTitle} foi removido da lista`
+
+    warningIcon.src = "assets/warning.svg"
     
-    xIcon.src = "assets/delete-small.png"
+    xIcon.src = "assets/delete-small.svg"
     xIcon.classList.add("removed-item-icon")
 
-    listItem.append(warningIcon, removalText, xIcon)
+    removeDiv.append(warningIcon, removalText)
+    listItem.append(removeDiv, xIcon)
 
     setTimeout(() => {
         listItem.remove()
@@ -65,4 +80,8 @@ function removedItem(button) {
     xIcon.onclick = () => {
         listItem.remove()
     }
+}
+
+function clearForm() {
+    input.value = ""
 }
